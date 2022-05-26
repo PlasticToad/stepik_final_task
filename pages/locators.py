97ea37e7bca7553f9e537a6de4eb24d73e2fafdb
User0 <0@0>
2022-05-26 20:11:13 +1000

    step 7. 10 tests added: 9 passed, 1 failed

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+	
+class ProductPageLocators():#уникальные селекторы
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME = (By.CSS_SELECTOR, ".product_main > h1")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    BOOK_NAME_FOR_CHECK = (By.CSS_SELECTOR, "#messages > .alert-success:nth-child(1) > .alertinner strong")
+    BOOK_PRICE_FOR_CHECK = (By.CSS_SELECTOR, "#messages > .alert-info > .alertinner p:nth-child(1) strong") 
---
+++ pages/product_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+import time
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_to_basket = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket.click()
+        self.solve_quiz_and_get_code()		
+		
+    def should_be_same_book_name(self):#проверка совпадения названия книги с названием в отчете о заказе
+        book_name_for_check = self.browser.find_element(*ProductPageLocators.BOOK_NAME_FOR_CHECK)
+        print("Order confirmed: " + book_name_for_check.text)#для наглядности в консоли
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME)
+        print("Book name: " + book_name.text)#для наглядности в консоли
+        assert book_name.text == book_name_for_check.text, "Wrong name!"
+
+    def should_be_same_book_price(self):#проверка совпадения цены с суммой в корзине
+        book_price_for_check = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_FOR_CHECK)
+        print("Price confirmed: " + book_price_for_check.text)#для наглядности в консоли
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        print("Book price: " + book_price.text)#для наглядности в консоли
+        assert book_price.text == book_price_for_check.text, "Wrong price!"
